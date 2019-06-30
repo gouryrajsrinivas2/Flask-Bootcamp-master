354bb730afb5273372654ed353f3196a33b8f2bb
gouryrajsrinivas2 <gouryrajsrinivas2@gmail.com>
2019-07-01 00:23:01 +0530

    python2 done

--- 01-Python/Python_Level_Two/02-OOP_Project.py
+++ 01-Python/Python_Level_Two/02-OOP_Project.py
@@ -19,11 +19,22 @@
 # Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
 
 
+    
 
 
 class Account:
-    pass
-
+    def __init__(self,owner,balance):
+        self.owner=owner
+        self.balance=balance
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+    def withdraw(self,amount):
+        if(self.balance<amount):
+            print("sorry your balance is less than requested")
+        else:
+            self.balance=self.balance-amount
+    def __repr__(self):
+        return(f"owner of the account is {self.owner} and balence is {self.balance}")
 
 # 1. Instantiate the class
 acct1 = Account('Jose',100)
@@ -36,25 +47,25 @@ print(acct1)
 
 
 # 3. Show the account owner attribute
-acct1.owner
+print(acct1.owner)
 
 
 
 
 # 4. Show the account balance attribute
-acct1.balance
+print(acct1.balance)
 
 
 
 
 # 5. Make a series of deposits and withdrawals
 acct1.deposit(50)
-
+print(acct1.balance)
 
 
 
 acct1.withdraw(75)
-
+print(acct1.balance)
 
 
 
