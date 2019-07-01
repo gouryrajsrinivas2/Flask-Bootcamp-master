1cc5d2da8384cabdce2340ec7f465b4c03e24752
gouryrajsrinivas2 <gouryrajsrinivas2@gmail.com>
2019-07-02 02:13:17 +0530

    templtes and forms done

--- 03-Template-Basics/07-Simple-Project.py
+++ 03-Template-Basics/07-Simple-Project.py
@@ -1,14 +1,26 @@
 # Set up your imports and your flask app.
+from flask import Flask,render_template,request
 
+app=Flask(__name__)
 @app.route('/')
 def index():
     # This home page should have the form.
-    pass
+    return render_template('exercise_home_page.html')
 
 
 # This page will be the page after the form
 @app.route('/report')
 def report():
+    username=request.args.get('first')
+    uppercase=False
+    lowercase=False
+    lastdigit=False
+
+    uppercase=any(c.isupper() for c in username)
+    lowercase=any(c.islower() for c in username)
+    lastdigit=username[-1:].isdigit()
+
+    return render_template('exercise_feedback_page.html',uppercase=uppercase,lowercase=lowercase,lastdigit=lastdigit)
     # Check the user name for the 3 requirements.
 
     # HINTS:
@@ -16,8 +28,8 @@ def report():
     # https://stackoverflow.com/questions/26515422/how-to-check-if-last-character-is-integer-in-raw-input
 
     # Return the information to the report page html.
-    pass
+    
 
 if __name__ == '__main__':
     # Fill this in!
-    pass
+    app.run(debug=True)
